18a41c80054402a38485cfd4d356d519e4e84077
Suzan Dewitz <suzanyaa@yahoo.com>
2022-12-27 17:10:49 +0000

    Update readme

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 import random
+# For selecting a random word
 from words import words
+import string
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
 
+    return word
 
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
 
-
+User_input = input('Type something:')
+print(user_input)
