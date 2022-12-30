b72980e65d9b04f035e5f107af1e09a1d8c12d77
Suzan Dewitz <suzanyaa@yahoo.com>
2022-12-30 12:00:34 +0000

    Add new line

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 from words import words
-from hangman_lives import hangman_lives
+
 from time import sleep
 import string
 
@@ -11,11 +11,92 @@ def get_valid_word(words):
 
     return word
 
-def hangman():
-    word = get_valid_word(words)
-    word_letters = set(word)  # letters in the word
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what the user has guessed
+import random
 
-User_input = input('Type something:')
-print(user_input)
+
+
+# Choose a random word
+word = random.choice(words)
+
+# Initialize variables
+guesses = []
+max_guesses = 8
+current_guesses = 0
+
+# List of images for different stages of the game
+images = [
+    """
+    +---+
+        |
+        |
+        |
+       ===""",
+    """
+    +---+
+    O   |
+        |
+        |
+       ===""",
+    """
+    +---+
+    O   |
+    |   |
+        |
+       ===""",
+    """
+    +---+
+    O   |
+   /|   |
+        |
+       ===""",
+    """
+    +---+
+    O   |
+   /|\  |
+        |
+       ===""",
+    """
+    +---+
+    O   |
+   /|\  |
+   /    |
+       ===""",
+    """
+    +---+
+    O   |
+   /|\  |
+   / \  |
+       ===""",
+]
+
+# Display the initial state of the game
+print(images[0])
+print("Welcome to Hangman!")
+print(f"You have {max_guesses} guesses to guess the word.")
+
+# Main game loop
+while True:
+    # Print the current state of the word
+    current_word = ""
+    for letter in word:
+        if letter in guesses:
+            current_word += letter
+        else:
+            current_word += "_"
+    print(current_word)
+
+    # Check if the player has won or lost
+    if current_word == word:
+        print("Congratulations! You won!")
+        break
+    elif current_guesses >= max_guesses:
+        print("Sorry, you lost. The word was:", word)
+        break
+
+    # Get the player's guess
+    guess = input("Guess a letter: ")
+    guesses.append(guess)
+    current_guesses += 1
+
+    # Display the current image
+    print(images[current_guesses])
